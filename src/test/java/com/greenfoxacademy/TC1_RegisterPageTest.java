e4d98d0a9319ceca7703805051e3fc7da7094ba4
DeadlyFr0sT <DeadlyFr0sT@users.noreply.github.com>
2021-08-05 00:06:24 +0200

    added TC1_RegisterPageTest - Registration tests done

---
+++ src/test/java/com/greenfoxacademy/TC1_RegisterPageTest.java
@@ -0,0 +1,59 @@
+package com.greenfoxacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.greenfoxacademy.pages.HomePage;
+import com.greenfoxacademy.pages.RegisterPage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC1_RegisterPageTest extends BaseTest {
+  HomePage homePage;
+  RegisterPage registerPage;
+
+  @BeforeEach
+  public void setup() {
+    homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.open();
+    registerPage = PageFactory.initElements(driver, RegisterPage.class);
+  }
+
+  @Test
+  @DisplayName("Unsuccessful registration")
+  @Feature("Registration")
+  @Description("Unsuccessful registration due to non-acceptance of privacy policy")
+  public void registrationShouldFailWithoutCheckPrivacyCheckbox() {
+    homePage.getLoginDropDown().click();
+    homePage.getRegisterLink().click();
+    registerPage.getUsernameField().sendKeys("TestUser1");
+    registerPage.getEmailField().sendKeys("tibortest00+reg@gmail.com");
+    registerPage.getNewPassword().sendKeys("Jelszo01");
+    registerPage.getConfirmPassword().sendKeys("Jelszo01");
+    registerPage.getRegisterButton().click();
+    assertThat(registerPage.getFailMessage().getText()).isEqualTo("This field is required");
+  }
+
+  @Test
+  @DisplayName("Successful registration")
+  @Feature("Registration")
+  @Description("Successfully registration after open and read the Privacy Policy")
+  public void registrationShouldSuccess() {
+    homePage.getLoginDropDown().click();
+    homePage.getRegisterLink().click();
+    registerPage.getPrivacyLink().click();
+    assertThat(driver.getTitle()).isEqualTo("Community Wiki | Privacy Policy");
+    driver.navigate().back();
+    registerPage.getUsernameField().sendKeys("TestUser1");
+    registerPage.getEmailField().sendKeys("tibortest00+reg@gmail.com");
+    registerPage.getNewPassword().sendKeys("Jelszo01");
+    registerPage.getConfirmPassword().sendKeys("Jelszo01");
+    registerPage.getPrivacyCheckbox().click();
+    registerPage.getRegisterButton().click();
+    assertThat(registerPage.getSuccessMessage().getText())
+        .isEqualTo("Thank you for your registration. You may log in now.");
+  }
+}
