1179eef4934a2708c5ed7796eed467cd515ce476
DeadlyFr0sT <DeadlyFr0sT@users.noreply.github.com>
2021-08-05 19:26:03 +0200

    add TC7_DataDeleteTest - Data delete test done

---
+++ src/test/java/com/greenfoxacademy/TC7_DataDeleteTest.java
@@ -0,0 +1,46 @@
+package com.greenfoxacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.greenfoxacademy.pages.EditPage;
+import com.greenfoxacademy.pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC7_DataDeleteTest extends BaseTest {
+  HomePage homePage;
+  EditPage editPage;
+
+  @BeforeEach
+  public void setup() {
+    editPage = PageFactory.initElements(driver, EditPage.class);
+    homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.open();
+    homePage.getLoginDropDown().click();
+    homePage.getLoginUsernameField().sendKeys("TestUser1");
+    homePage.getLoginPasswordField().sendKeys("Jelszo01");
+    homePage.getLoginButton().click();
+  }
+
+  @Test
+  @DisplayName("TC_10-DataDelete")
+  @Feature("Data Delete")
+  @Description("Delete the previously modified Wiki Page content")
+  public void shouldModifyWikiPageContent() {
+    String wikiPageName = "User1-page";
+    homePage.getQuickEditField().sendKeys(wikiPageName);
+    homePage.getQuickEditCreateButton().click();
+    editPage.getEditField().clear();
+    editPage.getSaveButton().click();
+    String actualBackgroundColor = editPage
+        .getSuccessMessage().getCssValue("background-color");
+    String expectedBackgroundColor = "rgba(212, 237, 218, 1)";
+    assertThat(actualBackgroundColor).isEqualTo(expectedBackgroundColor);
+    assertThat(editPage.getSuccessMessageText().getText()).contains("To review the changes");
+    homePage.logout();
+  }
+}
