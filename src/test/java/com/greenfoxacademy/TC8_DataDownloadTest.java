c9e78465a1aaa677fa28772b0a3b53a67c1a5f75
DeadlyFr0sT <DeadlyFr0sT@users.noreply.github.com>
2021-08-05 20:52:26 +0200

    add TC8_DataDownloadTest and By-The-Numbers.txt - Data download test done

---
+++ src/test/java/com/greenfoxacademy/TC8_DataDownloadTest.java
@@ -0,0 +1,41 @@
+package com.greenfoxacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.greenfoxacademy.pages.CommunityPage;
+import com.greenfoxacademy.pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC8_DataDownloadTest extends BaseTest {
+  HomePage homePage;
+  CommunityPage communityPage;
+
+  @BeforeEach
+  public void setup() {
+    communityPage = PageFactory.initElements(driver, CommunityPage.class);
+    homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.open();
+    homePage.getLoginDropDown().click();
+    homePage.getLoginUsernameField().sendKeys("TestUser1");
+    homePage.getLoginPasswordField().sendKeys("Jelszo01");
+    homePage.getLoginButton().click();
+  }
+
+  @Test
+  @DisplayName("TC_11-SaveDataToFile")
+  @Feature("Data Save")
+  @Description("Save By the numbers list elements to By-The-Numbers.txt")
+  public void shouldSaveByTheNumbersListElementsToTxt() {
+    communityPage.getAboutMenuLink().click();
+    communityPage.getAboutMenuCommunityItemLink().click();
+    communityPage.saveListElementsToTxt();
+    homePage.logout();
+    assertThat(driver.getCurrentUrl())
+        .isEqualTo("http://test-automation-wiki2.greenfox.academy/tiki-index.php");
+  }
+}
