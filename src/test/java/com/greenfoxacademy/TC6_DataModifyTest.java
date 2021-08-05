909063fed5ca25d1a385e3db30612db281a16d0c
DeadlyFr0sT <DeadlyFr0sT@users.noreply.github.com>
2021-08-05 19:24:41 +0200

    add TC6_DataModifyTest - Data modify test done

---
+++ src/test/java/com/greenfoxacademy/TC6_DataModifyTest.java
@@ -0,0 +1,48 @@
+package com.greenfoxacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.greenfoxacademy.pages.EditPage;
+import com.greenfoxacademy.pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC6_DataModifyTest extends BaseTest {
+  HomePage homePage;
+  EditPage editPage;
+
+  @BeforeEach
+  public void setup() {
+    editPage = PageFactory.initElements(driver, EditPage.class);
+    homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.open();
+    homePage.getLoginDropDown().click();
+    homePage.getLoginUsernameField().sendKeys("TestUser1");
+    homePage.getLoginPasswordField().sendKeys("Jelszo01");
+    homePage.getLoginButton().click();
+  }
+
+  @Test
+  @DisplayName("TC_09-DataModify")
+  @Feature("Data Modify")
+  @Description("Modify the previously created Wiki Page with specified content")
+  public void shouldModifyWikiPageContent() {
+    String wikiPageName = "User1-page";
+    String content = "I modified the content.";
+    homePage.getQuickEditField().sendKeys(wikiPageName);
+    homePage.getQuickEditCreateButton().click();
+    editPage.getEditField().clear();
+    editPage.getEditField().sendKeys(content);
+    editPage.getSaveButton().click();
+    String actualBackgroundColor = editPage
+        .getSuccessMessage().getCssValue("background-color");
+    String expectedBackgroundColor = "rgba(212, 237, 218, 1)";
+    assertThat(actualBackgroundColor).isEqualTo(expectedBackgroundColor);
+    assertThat(editPage.getSuccessMessageText().getText()).contains("To review the changes");
+    homePage.logout();
+  }
+}
