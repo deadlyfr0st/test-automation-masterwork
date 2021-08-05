0f255705c3577627f3eb8f3e0b1f071a67407dc9
DeadlyFr0sT <DeadlyFr0sT@users.noreply.github.com>
2021-08-05 16:38:33 +0200

    add TC3_CommunityPageTest - Simple data list done

---
+++ src/test/java/com/greenfoxacademy/TC3_CommunityPageTest.java
@@ -0,0 +1,41 @@
+package com.greenfoxacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.greenfoxacademy.pages.CommunityPage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC3_CommunityPageTest extends BaseTest {
+  CommunityPage communityPage;
+
+  @BeforeEach
+  public void setup() {
+    communityPage = PageFactory.initElements(driver, CommunityPage.class);
+    communityPage.open();
+  }
+
+  @Test
+  @DisplayName("TC_05-List")
+  @Feature("Data Listing")
+  @Description("Compare By the numbers list elements with the created list")
+  public void byTheNumbersListElementsShouldEqualWithManuallyCreatedList() {
+    communityPage.getAboutMenuLink().click();
+    communityPage.getAboutMenuCommunityItemLink().click();
+    List<String> expectedList = new ArrayList<>();
+    expectedList.add("10+ years old");
+    expectedList.add("40+ languages");
+    expectedList.add("250+ code contributors (via CVS/SVN)");
+    expectedList.add("1,500+ built-in features and preferences");
+    expectedList.add("1,000+ wiki pages of documentation");
+    expectedList.add("1,000,000+ downloads");
+    expectedList.add("1,000,000+ lines of code, with a code commit every two hours (on average)");
+    assertThat(communityPage.byTheNumbersRows()).isEqualTo(expectedList);
+  }
+}
